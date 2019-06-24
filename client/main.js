3a689009abc357e5e183592a6823f815d51ee8b1
Adi Laiman <44214543+adilaiman@users.noreply.github.com>
2019-06-24 22:33:25 +0100

    Add reminders to collection via form

--- client/main.js
+++ client/main.js
@@ -4,7 +4,36 @@ import { Reminders } from '../imports/api/reminders.js';
 import './main.html';
 
 Template.body.helpers({
+
   reminders() {
     return Reminders.find({});
   },
+
+});
+
+Template.body.events({
+
+  "submit .new-reminder" (e) {
+    // don't do default action
+    e.preventDefault();
+
+    // place title and descrip elements into vars
+    const t = document.querySelector("#title");
+    const d = document.querySelector("#description");
+    // get values from t,d
+    const tV = t.value;
+    const dV = d.value;
+
+    // insert data into collection
+    Reminders.insert({
+      title: tV,
+      description: dV,
+      date: new Date()
+    });
+
+    // clear form values
+    t.value = "";
+    d.value = "";
+  },
+
 });
