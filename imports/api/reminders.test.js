af9127ce35bdc679dc7ad8715eaaf74e14543d90
Adi Laiman <44214543+adilaiman@users.noreply.github.com>
2019-06-25 20:20:05 +0100

    Implement basic tests

---
+++ imports/api/reminders.test.js
@@ -0,0 +1,40 @@
+import { Meteor } from "meteor/meteor";
+import { Random } from 'meteor/random';
+import { assert } from 'meteor/practicalmeteor:chai';
+
+import { Reminders } from './reminders.js';
+
+if (Meteor.isServer) {
+    describe('Rreminders', () => {
+        describe('methods', () => {
+            const userId = Random.id();
+            let reminderId;
+            beforeEach(() => {
+                Reminders.remove({});
+                reminderId = Tasks.insert({
+                  title: 'test title',
+                  date: new Date(),
+                  owner: userId,
+                  username: 'tmeasday',
+                });
+              });
+
+            it('can delete owned task', () => {
+                // Find the internal implementation of the task method so we can
+
+                // test it in isolation
+                const deleteReminders = Meteor.server.method_handlers['reminders.remove'];
+
+                // Set up a fake method invocation that looks like what the method expects
+
+                const invocation = { userId };
+
+                // Run the method with `this` set to the fake invocation
+                deleteReminders.apply(invocation, [reminderId]);
+
+                // Verify that the method does what we expected
+                assert.equal(Reminders.find().count(), 0);
+            });
+        });
+    });
+}
